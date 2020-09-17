153c4827177ff5e257f366ef974130cf70cf5761
Aaron Centeno <68842941+aaroncenteno@users.noreply.github.com>
2020-09-16 19:43:41 -0700

    facility cards updated (#12)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,28 @@
-$(document).ready(function(){
-    $('#modal1').modal();
+$(document).ready(function () {
+  $('#modal1').modal();
+  
+  $('input.autocomplete').autocomplete({
+    data: {
+      "Apple": null,
+      "Microsoft": null,
+      "Google": 'https://placehold.it/250x250'
+    },
+  });
 
-    var data = {
-        resource_id: '5b6ab22d-42d4-4049-b068-067aacf4c7b3', // the resource id
-        limit: 5, // get 5 results
-        q: 'jones' // query for 'jones'
-      };
-      $.ajax({
-        url: 'https://data.ca.gov/api/3/action/datastore_search',
-        data: data,
-        dataType: 'jsonp',
-        success: function(data) {
-          alert('Total results found: ' + data.result.total)
-          console.log(data);
-        }
-      });
-
-
-});
-
+//   var data = {
+//     resource_id: 'b6648a0d-ff0a-4111-b80b-febda2ac9e09', // the resource id
+//     limit: 5, // get 5 results
+//     q: 'jones' // query for 'jones'
+//   };
+//   $.ajax({
+//     url: '',
+//     data: data,
+//     dataType: 'jsonp',
+//     success: function(data) {
+//       alert('Total results found: ' + data.result.total)
+//     }
+//   });
+})
 
 var covidCasesApi = "cf11de0d-32c5-451a-bfd1-dd7b1951978a";
 var ctx = document.getElementById('myChart').getContext('2d');
@@ -29,35 +33,11 @@ var myChart = new Chart(ctx, {
         datasets: [{
             label: '# of Cases',
             data: [50, 90, 150, 300, 450, 500, 1000, 1250, 1600, 1900, 2000,],
-            backgroundColor: [
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-                'rgba(101, 150, 255, 0.2)',
-            ],
-            borderColor: [
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-                'rgba(45, 101, 255, 1)',
-            ],
-            borderWidth: 1
+            backgroundColor: 
+                'rgba(128, 203, 196, 0.4)',
+            borderColor: 
+                'rgba(0, 96, 100, 1)',
+             borderWidth: 1,
         }]
     },
   
@@ -81,39 +61,3 @@ var myChart = new Chart(ctx, {
     }
 });
 
-
-$(document).ready(function () {
-// modal was triggered
-$("#task-form-modal").on("show.bs.modal", function() {
-    // clear values
-    $("#modalTaskDescription, #modalDueDate").val("");
-  });
-  
-  // modal is fully visible
-  $("#task-form-modal").on("shown.bs.modal", function() {
-    // highlight textarea
-    $("#modalTaskDescription").trigger("focus");
-  });
-  
-  // save button in modal was clicked
-  $("#task-form-modal .btn-save").click(function() {
-    // get form values
-    var taskText = $("#modalTaskDescription").val();
-    var taskDate = $("#modalDueDate").val();
-  
-    if (taskText && taskDate) {
-      createTask(taskText, taskDate, "toDo");
-  
-      // close modal
-      $("#task-form-modal").modal("hide");
-  
-      // save in tasks array
-      tasks.toDo.push({
-        text: taskText,
-        date: taskDate
-      });
-  
-      saveTasks();
-    }
-  });
-})
