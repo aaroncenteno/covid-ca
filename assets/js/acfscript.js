981db274fb185ddc2f8e0cb91d8d09e7bed354d0
ASC8 <asamcent@gmail.com>
2020-09-23 19:13:30 -0700

    cleaned up code

--- assets/js/acfscript.js
+++ assets/js/acfscript.js
@@ -1,103 +1,42 @@
-var locAction = document.querySelector(".loc-action");
-var locContainer = document.querySelector(".loc-container");
-var locList = document.querySelector(".loc-list");
-var chartEl = document.querySelector("#chart-container");
-var cities = [];
-var cityCasesEl = document.querySelector(".city-cases");
-var cityDeathsEl = document.querySelector(".city-deaths");
-var cityHeaderEl = document.querySelector(".city-header");
-var cityInputEl = document.querySelector("#city-search");
-var cityList = document.querySelector(".collection");
-//var cntyName = cityInputEl.value.trim();
-//var cntyName = "Los Angeles"
-var currentCity = document.querySelector(".current-city");
-var dataBaseInfo = [];
-var deleteButtonEl = document.querySelector("#clear-storage-btn")
+var cntyAction = document.querySelector(".cnty-action");
+var cntyContainer = document.querySelector(".cnty-container");
+var cntyList = document.querySelector(".cnty-list");
+var cntyList = document.querySelector(".collection");
+var currentcnty = document.querySelector(".current-cnty");
 var facilitiesHeader = document.querySelector(".facilities-header");
 var navBtn = document.querySelector("#navigate-button");
 var navContent = document.querySelector(".modal-content");
 var navHeader = document.querySelector(".facility-name");
 var navMap = document.querySelector("#nav-map");
-var searchButtonEl = document.querySelector("#searchbutton");
-var faciltyName = document.querySelector(".loc-title");
-var testingLoc = document.querySelector("#testingLoc");
+var testingcnty = document.querySelector("#testingcnty");
 
-
+// Modals For Map and Error
 $(document).ready(function () {
-  $('#modal1').modal();
   $('#modal2').modal();
   $('#modal3').modal();
-
 });
   
+// Grab County Name and Format for Display
 var getCntyName = function() {
     // grab repo name from url query string
     var queryString = document.location.search;
     var cntyName = queryString.split("=")[1];
   
     if (cntyName) {
-      // display repo name on the page
-      testingLoc.textContent=cntyName.replace(/%20/g, " ");
-      searchHistory(cntyName);
-  
-      //getRepoIssues(cntyName);
+      // display county name on the page
+      testingcnty.textContent=cntyName.replace(/%20/g, " ");
+      listFacilities(cntyName);
     } else {
       // if no repo was given, redirect to the homepage
-      document.location.replace("./index.html");
+      document.cntyation.replace("./index.html");
     }
-    
 };
 
-//function to grab user's city search choice
-var searchButtonHandler = function (event) {
-  //prevent browser from sending user's input data to a URL
-  // event.preventDefault();
-  //get value
-  var cntyName = cityInputEl.value.trim();
-  var splitStr = cntyName.toLowerCase().split(' ');
-  for (var i = 0; i < splitStr.length; i++) {
-    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-  }
-  // Directly return the joined string
-  cntyName = splitStr.join(' ');
-
-  //add cntyName to list
-  if (cntyNames[cntyName] === null) {
-    //make sure cntyName is one of the counties on the drop down list (if (cntyName === data[i]????))
-    //reset cityInput
-    locContainer.innerHTML = "";
-    // cityInputEl.value = "";
-    chartEl.innerHTML= "";
-    appendCity(cntyName);
-    //add to local storage
-    //saveCity(cntyName);
-    getCoordinates(cntyName);
-    //getResults(cntyName);
-  }
-}  
-var searchHistory = function (cntyName) {
-  
+var listFacilities = function (cntyName) {
   if (cntyName) {
-    //cityInputEl.value = "";
-   // locContainer.textContent = "";
-    //chartEl.innerHTML = "";
     getCoordinates(cntyName);
-   // getResults(cntyName);
   }
 }
-
-//function to add city to list
-var appendCity = function (cntyName) {
-  if (cities.indexOf(cntyName) === -1) {
-    //append to list
-    var cityItem = document.createElement("li");
-    // cityItem.classList
-    cityItem.textContent = cntyName;
-    cityList.appendChild(cityItem);
-  }
-}
-
-
 
 //FUNCTION to convert cntyName into Long/Lat coordinates
 var getCoordinates = function (cntyName) {
@@ -107,91 +46,81 @@ var getCoordinates = function (cntyName) {
       return response.json();
     })
     .then(function (data) {
-      var cityLatitude = data.results[0].geometry.location.lat;
-      var cityLongitude = data.results[0].geometry.location.lng;
-      getTestSites(cityLatitude, cityLongitude);
+      var cntyLatitude = data.results[0].geometry.location.lat;
+      var cntyLongitude = data.results[0].geometry.location.lng;
+      getTestSites(cntyLatitude, cntyLongitude);
     })
 
 }
 
-//hardcoding testing site API until we have a drop down select menu for city
-var getTestSites = function (cityLatitude, cityLongitude) {
-  var testingApiUrl = "https://discover.search.hereapi.com/v1/discover?apikey=X0SijTp9QmtmfIHB8-dU1wKqKEFl9qFxGxhIhiG1_b0&q=Covid&at=" + cityLatitude + "," + cityLongitude ;
+// Fetch Testing Facilities Using Lat and Lng and Display in List
+var getTestSites = function (cntyLatitude, cntyLongitude) {
+  var testingApiUrl = "https://discover.search.hereapi.com/v1/discover?apikey=X0SijTp9QmtmfIHB8-dU1wKqKEFl9qFxGxhIhiG1_b0&q=Covid&at=" + cntyLatitude + "," + cntyLongitude ;
   fetch(testingApiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      locContainer.innerHTML = "";
+      cntyContainer.innerHTML = "";
       for (var i = 0; i < data.items.length; i++) {
-        //add testing center title to locs, add testing address to locs
-        var locTitle = document.createElement("span");
-        var locContent = document.createElement("div");
-        var loc = document.createElement("div");
-        var locAddress = document.createElement("a");
-        var locBody = document.createElement("div");
-        facilitiesHeader.classList.remove("hide");
-        //currentCity.classList.remove("hide");
-        locBody.classList = "loc-action";
-        locAddress.classList = "facility-address modal-trigger";
-        locAddress.id = "facility-address";
-        locAddress.textContent = " " + data.items[i].address.houseNumber + " " + data.items[i].address.street + ", " + data.items[i].address.county + ", " + data.items[i].address.state + " " + data.items[i].address.postalCode;
-        console.log(locAddress.textContent);
-        locAddress.setAttribute("href", "#modal2") ;
-        locTitle.classList = "loc-title";
-        locTitle.textContent = data.items[i].title.split(":")[1];
-        console.log(locTitle.textContent);
-        locContent.classList = "loc-content white-text";
-        loc.classList = "loc darken-1 row loc-item";
-        var cardEl=document.createElement("div");
-        var cardLat=document.createElement("a");
-        cardEl.classList = "hide"
-        cardLat.textContent=data.items[i].position.lat + "," + data.items[i].position.lng;
-        // console.log(cardLat.textContent);
-        locContent.appendChild(cardEl);
-        cardEl.appendChild(cardLat);
+
+        //add testing center title to cntys, add testing address to cntys
+        var cntyTitle = document.createElement("span");
+        var cntyContent = document.createElement("div");
+        var cnty = document.createElement("div");
+        var cntyAddress = document.createElement("a");
+        var cntyBody = document.createElement("div");
+        var cntyEl=document.createElement("div");
+        var cntyLat=document.createElement("a");
+
         
-        //console.log(locTitle.textContent);
-        //append loc title and address to the page
-        locContent.appendChild(locTitle);
-        locContent.appendChild(locAddress);
-        locBody.appendChild(locContent);
-        loc.appendChild(locBody);
-        locList.appendChild(loc);
-        locContainer.appendChild(locList);
+
+        // Check for House Number, Street to Remove Undefined from Text Content
+        if (data.items[i].address.houseNumber) {
+          cntyAddress.textContent = " " + data.items[i].address.houseNumber + " " + data.items[i].address.street + ", " + data.items[i].address.city + ", " + data.items[i].address.state + " " + data.items[i].address.postalCode;
+          } else if (data.items[i].address.street) {
+            cntyAddress.textContent = " " + data.items[i].address.street + ", " + data.items[i].address.city + ", " + data.items[i].address.state + " " + data.items[i].address.postalCode;
+          } else {
+            cntyAddress.textContent = " " + data.items[i].address.city + ", " + data.items[i].address.state + " " + data.items[i].address.postalCode;
+          }
+        
+        // Set Attribute of List Items
+        facilitiesHeader.classList.remove("hide");
+        cntyBody.classList = "cnty-action";
+        cntyAddress.classList = "facility-address modal-trigger";
+        cntyAddress.id = "facility-address";
+        cntyAddress.setAttribute("href", "#modal2") ;
+        cntyTitle.classList = "cnty-title";
+        cntyTitle.textContent = data.items[i].title.split(":")[1];
+        cntyContent.classList = "cnty-content white-text";
+        cnty.classList = "cnty darken-1 row cnty-item";
+        cntyEl.classList = "hide"
+        cntyLat.textContent=data.items[i].position.lat + "," + data.items[i].position.lng;
+        
+        // Append List Items to List and Display
+        cntyContent.appendChild(cntyEl);
+        cntyEl.appendChild(cntyLat);
+        cntyContent.appendChild(cntyTitle);
+        cntyContent.appendChild(cntyAddress);
+        cntyBody.appendChild(cntyContent);
+        cnty.appendChild(cntyBody);
+        cntyList.appendChild(cnty);
+        cntyContainer.appendChild(cntyList);
       }
     })
+
     // Set Data for Embedded Map and Navigate Button
     $(document).on("click", ".facility-address", function() {
-    // console.log($(this.parentElement.parentElement.children[0].children[0].innerHTML).text());
     var latLng = $(this.parentElement.parentElement.children[0].children[0].innerHTML).text();
     var mapFacility=$(this.parentElement.parentElement.children[0].children[1]).text();
+
     navHeader.textContent=mapFacility;
+
     navBtn.setAttribute("target", "_blank");
     navBtn.setAttribute("href", "https://www.google.com/maps/search/?api=1&query=" + latLng);
     navMap.setAttribute("src", "https://www.google.com/maps/embed/v1/place?q=" + latLng + "&key=AIzaSyAbDIvcfoHMHKqc3Qo-TB3OGNGoRBGTUJo");
-    // console.log(cardAddress.val);
     });
 }
-
-// // Set Data for Embedded Map and Navigate Button
-
-// $(document).on("click", ".facility-address", function ($c) {
-//    //console.log($(this).text());
-//   navBtn.setAttribute("target", "_blank");
-//   navBtn.setAttribute("href", "https://www.google.com/maps/search/?api=1&query=" + $(this).text());
-//   navMap.setAttribute("src", "https://www.google.com/maps/embed/v1/place?&key=AIzaSyAbDIvcfoHMHKqc3Qo-TB3OGNGoRBGTUJo&q=" + $(this).text());
-// })
-
-$(document).on("click", ".collection-item", function () {
-  // console.log($(this).text());
-  getCoordinates($(this).text());
-  //getResults($(this).text());
-});
-
-$(".collection").on("click", "li", function () {
-  searchHistory($(this).text());
-})
 
 getCntyName();
 
